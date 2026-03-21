56a02bf53e90f6f84ce1185396a74535d8fb12c2
Sebastian Blazinaric <sebastian.blazinaric@zalando.de>
2026-03-21 14:43:48 +0100

    cook

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+// Custom cursor
+const cursor = document.getElementById('cursor');
+const dot = document.getElementById('cursorDot');
+let mouseX = 0, mouseY = 0;
+let curX = 0, curY = 0;
 
+document.addEventListener('mousemove', e => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  dot.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
+});
+
+function animateCursor() {
+  curX += (mouseX - curX) * 0.12;
+  curY += (mouseY - curY) * 0.12;
+  cursor.style.transform = `translate(${curX}px, ${curY}px)`;
+  requestAnimationFrame(animateCursor);
+}
+animateCursor();
+
+document.querySelectorAll('.artwork').forEach(el => {
+  el.addEventListener('mouseenter', () => cursor.classList.add('cursor--hover'));
+  el.addEventListener('mouseleave', () => cursor.classList.remove('cursor--hover'));
+});
+
+// Staggered load animation
+document.querySelectorAll('.artwork').forEach((el, i) => {
+  el.style.animationDelay = `${0.1 + i * 0.15}s`;
+});
